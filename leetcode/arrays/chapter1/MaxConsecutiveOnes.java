88fea1a61280ef7edeae96b90fcc9cc0df173371
Gustavo Delgado <gustavodelgado@pm.me>
2022-03-11 12:02:34 -0300

    melhorando performance do algoritmo

--- leetcode/arrays/chapter1/MaxConsecutiveOnes.java
+++ leetcode/arrays/chapter1/MaxConsecutiveOnes.java
@@ -3,13 +3,15 @@ package leetcode.arrays.chapter1;
 public class MaxConsecutiveOnes {
     public int findMaxConsecutiveOnes(int[] nums) {
         int consecutiveOnes = 0;
+        int maxConsecutiveOnes = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
                 consecutiveOnes++;
             } else {
+                maxConsecutiveOnes = Math.max(maxConsecutiveOnes, consecutiveOnes);
                 consecutiveOnes = 0;
             }
         }
-        return consecutiveOnes;
+        return Math.max(maxConsecutiveOnes, consecutiveOnes);
     }
 }
