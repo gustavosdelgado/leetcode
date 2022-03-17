a62aac6c6cc0a6e4605ff511a7cd95f10ee33b49
Gustavo Delgado <gustavodelgado@protonmail.com>
2022-03-17 14:18:11 -0300

    commit de teste

--- leetcode/arrays/chapter4/CheckIfExists.java
+++ leetcode/arrays/chapter4/CheckIfExists.java
@@ -4,7 +4,6 @@ public class CheckIfExists {
     public static boolean checkIfExist(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int numberToCheck = arr[i];
-
             for (int j = 0; j < arr.length; j++) {
                 if (i != j && arr[i] * 2 == arr[j]) {
                     return true;
