c5e33bd891f53a082600b45e434bf0f67cd6d095
Gustavo Delgado <gustavodelgado@pm.me>
2022-03-17 14:13:04 -0300

    adding linear search exercise

---
+++ leetcode/arrays/chapter4/CheckIfExists.java
@@ -0,0 +1,18 @@
+package leetcode.arrays.chapter4;
+
+public class CheckIfExists {
+    public static boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int numberToCheck = arr[i];
+
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] * 2 == arr[j]) {
+                    return true;
+                }
+            }
+            
+        }
+        
+        return false;
+    }
+}
